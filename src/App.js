0c6cf60ccbe2dcd864f949586468d7f6d1f45bd3
Bhaviyav <bhavibhavishu@gmail.com>
2022-11-24 11:36:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React,{ useEffect, useState } from 'react';
+
+import { ClassNames } from '@emotion/react';
+
+import  AppBar  from './components/AppBar';
+import HomeCarousal from './components/HomeCarousal';
+import MovieSection from './components/MovieSection';
 
 function App() {
+ const [searchResult,setSearchResult] = useState("")
+ const handlesearchInput = (e)=>{
+      setSearchResult(e.target.value)
+      console.log(searchResult)
+  }
+  const carouselitems=["bg1.webp","bg2.webp","bg3.webp","bg4.webp","bg5.webp","bg6.webp"]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <AppBar setsearchinput={handlesearchInput}/>
+    {searchResult.length >= 4 &&  (<MovieSection title={"Search Result For '" + searchResult + "':"} Genre={searchResult}/>)}
+    <HomeCarousal Carouselitems={carouselitems}/>
+   
+    <MovieSection title="Trending Movies" Genre="hello"/>
+    <MovieSection title="Action" Genre="star"/>
+    <MovieSection title="Comedy" Genre="die"/>
+  
+   
+    
+    </>
   );
 }
-
 export default App;
